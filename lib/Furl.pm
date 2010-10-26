0b4ceafdfa10e6e2ad24ee8d32060242fd0af5f1
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2010-10-26 22:44:59 +0000

    Import of TOKUHIROM/Furl-0.04.tar.gz from CPAN.
    gitpan-cpan-distribution: Furl
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         TOKUHIROM/Furl-0.04.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Furl.pm
+++ lib/Furl.pm
@@ -2,7 +2,7 @@ package Furl;
 use strict;
 use warnings;
 use 5.008;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Carp ();
 use XSLoader;
@@ -258,6 +258,8 @@ sub request {
             select($sock); $|=1;
             select($orig);
         }
+
+        binmode $sock;
     }
 
     # write request
