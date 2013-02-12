673cd4aec167ac7df0c0aae15308a8b5f91716ae
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2013-02-12 06:03:07 +0000

    Import of TOKUHIROM/Furl-2.05.tar.gz from CPAN.
    gitpan-cpan-distribution: Furl
    gitpan-cpan-version:      2.05
    gitpan-cpan-path:         TOKUHIROM/Furl-2.05.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Furl/HTTP.pm
+++ lib/Furl/HTTP.pm
@@ -4,7 +4,7 @@ use warnings;
 use base qw/Exporter/;
 use 5.008001;
 
-our $VERSION = '2.04';
+our $VERSION = '2.05';
 
 use Carp ();
 use Furl::ConnectionCache;
@@ -858,7 +858,7 @@ sub _r500 {
     return(0, 500, "Internal Response: $message",
         [
             'Content-Length' => length($message),
-            'Client-Warning' => 'Internal Response',
+            'X-Internal-Response' => 1,
             # XXX ^^ EXPERIMENTAL header. Do not depend to this.
         ], $message
     );
